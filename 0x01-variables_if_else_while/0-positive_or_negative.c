7f5bcb7fcbfbebcd8bba9cf226eb9b244e27f66a
jessyovia <jessyovia100@gmail.com>
2023-08-11 21:08:27 -0700

    0 positive Nagative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: Assigns a random number to the variable n
+ *              and prints whether it is positive, negative, or zero.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+printf("%d is ", n);
+if (n > 0)
+printf("positive\n");
+else if (n == 0)
+printf("zero\n");
+else
+printf("negative\n");
+return (0);
+}
+
