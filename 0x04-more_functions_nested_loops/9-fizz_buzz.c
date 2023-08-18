391c1e246bbfe52e8c420d2edf0c1bd89f8c3376
jessyovia <jessyovia100@gmail.com>
2023-08-17 19:18:10 -0700

    9 Fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers from 1 to 100, replacing multiples of 3 and 5.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%d", i);
+}
+if (i != 100)
+{
+printf(" ");
+}
+}
+printf("\n");
+return (0);
+}
