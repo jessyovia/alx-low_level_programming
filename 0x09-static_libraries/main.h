b278c7e4e2d33d3d20cd374d755eeae876fc220b
jessyovia <jessyovia100@gmail.com>
2023-09-01 16:44:24 -0700

    0x09-static

---
+++ 0x09-static_libraries/main.h
@@ -0,0 +1,10 @@
+int _putchar(char c);
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int _strlen(char *s);
+int _atoi(char *s);
+int _strcmp(char *s1, char *s2);
