53a0e43b6abd1819be4fc8a1dfab7c31fc544b9b
jessyovia <jessyovia100@gmail.com>
2023-09-22 20:27:28 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * premain - function executed before main
+ */
+void __attribute__((constructor)) premain()
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
