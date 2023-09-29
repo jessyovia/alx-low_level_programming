18395c7662ebbfc64960e5d92094516cf9a2fff3
jessyovia <jessyovia100@gmail.com>
2023-09-28 20:03:47 -0700

    3-set_bit.c

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* get_bit - Returns the value of a bit at a given index..
+* @n: A pointer to the number to modify.
+* @index: The index of the bit to retrieve, starting from 0..
+*
+* Description: This function checks if the index is within the range of
+* of valid bits (0 to 63 on a typical 64-bit system).
+*
+* Return: The value of the bit at the given index or -1 if an error occurs.
+*/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 32) /* Ensure index is within a valid range (0-32) */
+		return (-1);
+
+	*n |= (1 << index); /* Set the bit at the specified index to 1 */
+
+	return (1);
+}
