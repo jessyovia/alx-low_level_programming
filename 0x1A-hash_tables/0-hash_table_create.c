bc088fb9fa6062630963d9511f5482b236c2f455
jessyovia <jessyovia100@gmail.com>
2023-12-22 19:06:00 -0800

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,29 +2,29 @@
 
 /**
  * hash_table_create - Creates a hash table.
- * @size: The size of the array.
+ * @size: The size of the hash table.
  *
- * Return: A pointer to the newly created hash table, or NULL if it fails.
+ * Return: A pointer to the newly created hash table, or NULL on failure.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *new_table = NULL;
+	hash_table_t *new_table;
+	unsigned long int index;
 
-    if (size < 1)
-        return (NULL);
+	new_table = malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
+		return (NULL);
 
-    new_table = malloc(sizeof(hash_table_t));
-    if (new_table == NULL)
-        return (NULL);
+	new_table->size = size;
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_table->array == NULL)
+	{
+		free(new_table);
+		return (NULL);
+	}
 
-    new_table->array = malloc(sizeof(hash_node_t *) * size);
-    if (new_table->array == NULL)
-    {
-        free(new_table);
-        return (NULL);
-    }
+	for (index = 0; index < size; index++)
+		new_table->array[index] = NULL;
 
-    new_table->size = size;
-
-    return (new_table);
+	return (new_table);
 }
