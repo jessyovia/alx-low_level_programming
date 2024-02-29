409c5a89192ac9a8d0faf387fc9e8812d6d09f7d
jessyovia <jessyovia100@gmail.com>
2024-02-28 21:05:46 -0800

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,22 +1,26 @@
-# 5-island_perimeter.py
-
 #!/usr/bin/python3
-"""
-5-island_perimeter
-"""
+"""Function returns the perimeter of the island described in grid"""
 
 
 def island_perimeter(grid):
     """
-    Returns the perimeter of the island described in grid
+    Calculate the perimeter of the island
+
+    Args:
+        grid: The grid representing the island
+
+    returns:
+        int: The perimeter of the island
     """
     perimeter = 0
+
     for i in range(len(grid)):
-        for j in range(len(grid[i])):
+        for j in range(len(grid[0])):
             if grid[i][j] == 1:
                 perimeter += 4
                 if i > 0 and grid[i - 1][j] == 1:
                     perimeter -= 2
                 if j > 0 and grid[i][j - 1] == 1:
                     perimeter -= 2
+
     return perimeter
